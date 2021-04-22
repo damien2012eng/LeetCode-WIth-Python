9035c164abeb73d23d84f825eda3b470f020815d
damien2012eng <damienchina2012@gmail.com>
2021-04-21 21:46:26 -0400

    Update Remove Duplicates from Sorted Array.py

--- Remove Duplicates from Sorted Array.py
+++ Remove Duplicates from Sorted Array.py
@@ -4,10 +4,12 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        #Initialization
         n = len(nums)
         if n == 0:
             return 0
         p1, p2 = 0, 1
+        #While loop with two pointers
         while p2 < n:
             if nums[p1] != nums[p2]:
                 p1 += 1
